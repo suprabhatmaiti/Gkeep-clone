650b2ac951bbc4ac172e752011fe179c6d11dda0
Suprabhat Maiti <maitisuprabhat@gmail.com>
2022-10-29 20:07:08 +0530

    initial node setup

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = 9000 || process.env.PORT;
+const keys = require("./src/config/keys");
+
+mongoose.connect(keys.mongoURI, () => {
+  console.log("mongoose connection established");
+});
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
 app.listen(PORT, () => {
-  console.log("app is listening to the port");
+  console.log(`app is listening to the port ${PORT}`);
 });
---
+++ src/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://admin:<password>@cluster0.scx1405.mongodb.net/?retryWrites=true&w=majority",
+};
