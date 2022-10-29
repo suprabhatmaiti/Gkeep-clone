9e00c685390f3d68f1b39cdd549a5b3d117ea956
Suprabhat Maiti <maitisuprabhat@gmail.com>
2022-10-29 20:14:00 +0530

    change the implementation using dotenv

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = 9000 || process.env.PORT;
-const keys = require("./src/config/keys");
 
-mongoose.connect(keys.mongoURI, () => {
+mongoose.connect(process.env.mongoURI, () => {
   console.log("mongoose connection established");
 });
 
--- src/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://admin:<password>@cluster0.scx1405.mongodb.net/?retryWrites=true&w=majority",
-};
